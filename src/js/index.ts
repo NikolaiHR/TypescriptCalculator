d74accee559cacf6628675cdfd4252974c27fdaa
Nikolai Holfelt Røssel <niko6307@edu.easj.dk>
2019-09-27 08:38:20 +0200

    Virker

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,12 @@
-interface Person {
-    firstName: string;
-    lastName: string;
-}
-
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
-
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+let knap: HTMLButtonElement = <HTMLButtonElement>document.getElementById("knap")
+knap.addEventListener("click", calculate)
+function calculate(): void {
+    let number1Field: HTMLInputElement = <HTMLInputElement>document.getElementById("number1")
+    let number2Field: HTMLInputElement = <HTMLInputElement>document.getElementById("number2")
+    let number1string: string = number1Field.value
+    let number2string: string = number2Field.value
+    let number1: number = Number(number1string)
+    let number2: number = Number(number2string)
+    let result: number = number1 + number2
+    let resultElement: HTMLInputElement = <HTMLInputElement>document.getElementById("result")
+    resultElement.innerHTML = result + ""}
